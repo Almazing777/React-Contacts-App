c4cf15cd9fcdb6a15cf478bfe65eea39010a02b6
almas <mendygax@gmail.com>
2019-05-16 16:59:24 -0400

    listcontacts.js update2

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -14,7 +14,7 @@ function ListContacts(props) {
                         <p>{contact.name}</p>
                         <p>{contact.handle}</p>
                     </div>
-                    <button className='contact-remove'>
+                    <button className='contact-removal'>
                         Remove!
                     </button>
                 </li>
